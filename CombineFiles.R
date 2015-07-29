040a26e610b4dfea1f4f785d5ee9ba59b6204b0a
baonwut <baoru@baoru-PC.corp.wxxr.com.cn>
2015-07-29 11:36:42 +0800

    refresh

--- CombineFiles.R
+++ CombineFiles.R
@@ -1,20 +1,23 @@
-##可以同时处理csv和txt两种格式
-##eg：a<-comfile(file="D:/test/",header=TRUE,sep="|","txt")
-##eg：a<-comfile(file="D:/test/",header=TRUE,sep="|","csv")
+##eg:a<-comfile(file="D:/test",header=TRUE,sep="|","txt")
+##eg:a<-comfile(file="D:/test",header=TRUE,sep="|","csv")
+
 
 comfile<-function(file,header,sep,format){
-        data<-data.frame()        
-        filename<-list.files(path=file,full.names=FALSE)
-        if(format=="txt") fun<-function(file,header,sep) 
-                read.table(file,header,sep)
-        else fun<-function(file,header,sep) 
-                read.csv(file,header,sep)
-        i<-1
-        for(i in i:length(filename)){
-                d<-fun(file=paste(file,filename[i],sep=""),
-                                 header=header,sep=sep)
-                data<-rbind(d,data)
-        }
-        write.table(data,file=paste(file,"all_data.txt",sep=""),sep=sep,
-                    quote=FALSE,row.names =FALSE)
+      #path
+      filename<-dir(path=file)
+      
+      #read txt or csv function
+      if(format=="txt") fun<-function(file,header,sep) 
+            read.table(file,header,sep)
+      else fun<-function(file,header,sep) 
+            read.csv(file,header,sep)
+      
+      #read file and combine
+      dt<-lapply(paste(file,filename,sep="/"),fun,header=header,sep=sep)
+      dt<-do.call(rbind,dt)
+      
+      #write
+      write.table(dt,file=paste(file,"/all_data.txt",sep=""),sep=sep,
+                  quote=FALSE,row.names =FALSE)
 }
+
